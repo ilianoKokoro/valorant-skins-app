861e877020cef64e9f753083677b19445d20bab8
ilianoKokoro <iliandelagrange@gmail.com>
2026-06-13 16:21:32 -0400

    Upgrade to latest next version

--- next.config.js
+++ next.config.js
@@ -1,25 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'media.valorant-api.com',
-        port: '',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.vecteezy.com',
-        port: '',
-        pathname: '**',
-      },
-    ],
-  },
-  env: {
-    HENRIKDEV_API_KEY: process.env.HENRIKDEV_API_KEY,
-  },
-}
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "media.valorant-api.com",
+                port: "",
+                pathname: "**",
+            },
+            {
+                protocol: "https",
+                hostname: "static.vecteezy.com",
+                port: "",
+                pathname: "**",
+            },
+        ],
+    },
+    env: {
+        HENRIKDEV_API_KEY: process.env.HENRIKDEV_API_KEY,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
